dc8404d2299cf3bff902b9a105e61eb35e15fa37
miro789 <mirohuyen5335.em@gmail.com>
2025-01-16 08:31:22 +0700

    Init project base with Spring Boot

---
+++ src/main/java/vn/miro/BackendServiceApplication.java
@@ -0,0 +1,24 @@
+package vn.miro;
+
+import org.springframework.beans.factory.annotation.Value;
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BackendServiceApplication {
+
+	@Value("${jwt.secretKey}")
+	private String jwtKey;
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendServiceApplication.class, args);
+	}
+
+	@PostConstruct
+	public void Test() {
+		System.out.println("jwtKey: " + jwtKey);
+	}
+
+}
---
+++ src/main/java/vn/miro/controller/HelloController.java
@@ -0,0 +1,20 @@
+package vn.miro.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+@Tag(name = "Hello Controller")
+public class HelloController {
+
+    @Operation(summary = "Test API", description = "This is the description part")
+    @GetMapping("/hello")
+    public String greeting(@RequestParam String name) {
+        return "Hello, " + name;
+    }
+}
